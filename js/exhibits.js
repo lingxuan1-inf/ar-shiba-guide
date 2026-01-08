acc3606691c6046400a3fcdc1974dc476d48b767
LingXuan <ohlingxuan@gmail.com>
2026-01-08 20:40:45 +0800

    Refine UI: Cat icon, fix rotation, update to Singapore Stone

--- js/exhibits.js
+++ js/exhibits.js
@@ -1,13 +1,13 @@
 const exhibits = {
     "default": {
         id: "intro",
-        title: "The Ancient Vase",
-        intro: "Woof! Look at this magnificent vase from the 14th century.",
+        title: "The Singapore Stone",
+        intro: "Welcome to the National Museum of Singapore. This is the legendary Singapore Stone.",
         details: [
-            "Use the button to learn more! Bark!",
-            "It was crafted by the grandmaster artisan of the Ming Dynasty.",
-            "Notice the intricate blue dragon patterns? That symbolizes power!",
-            "It was actually used to store grain, not flowers! Much wow!"
+            "This sandstone slab dates back to the 10th-14th century.",
+            "It was originally part of a massive boulder at the mouth of the Singapore River.",
+            "The inscription on it has never been fully deciphered.",
+            "It is one of the 11 National Treasures of Singapore."
         ]
     }
     // Add more exhibits here based on NFC URL parameters later
